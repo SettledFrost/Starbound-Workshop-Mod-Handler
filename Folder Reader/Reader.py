c0326780865eb30b46ed6ae53c7a59711657ffa0
SettledFrost <meidaskam.ltu@gmail.com>
2026-09-12 21:16:07 +0100

    Folder Reader Completed

--- Folder Reader/Reader.py
+++ Folder Reader/Reader.py
@@ -1,1 +1,20 @@
+import os
+
 from Config import Directories
+
+ReadDir = Directories["ReadDir"]
+
+def fetchFiles(Directory):
+    Files = os.listdir(Directory)
+    Files = [file for file in Files if os.path.isfile(Directory+'/'+file)]
+    return Files
+
+def fetchFilesByExtension(Directory, Extension):
+    FilesExt = []
+    Files = fetchFiles(Directory)
+    for file in Files:
+        if file.endswith(Extension):
+            FilesExt.append(file)
+    return FilesExt
+
+print(fetchFilesByExtension(ReadDir, ".txt"))
